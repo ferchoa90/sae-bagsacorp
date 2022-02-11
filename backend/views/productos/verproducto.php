3d45a24086572f8ce04e4b541788fef0fd6d8298
Mario Aguilar <marioaguilar1990@gmail.com>
2022-02-11 09:02:23 -0500

    produ
    prod

--- backend/views/productos/verproducto.php
+++ backend/views/productos/verproducto.php
@@ -32,22 +32,34 @@ $div= new Bloques;
        // array('tipo'=>'link','nombre'=>'guardar', 'id' => 'guardar', 'titulo'=>'&nbsp;Guardar', 'link'=>'', 'onclick'=>'' , 'clase'=>'', 'style'=>'', 'col'=>'', 'tipocolor'=>'verde', 'icono'=>'guardar','tamanio'=>'pequeño',  'adicional'=>''),
         array('tipo'=>'link','nombre'=>'regresar', 'id' => 'guardar', 'titulo'=>'&nbsp;Regresar', 'link'=>'', 'onclick'=>'history.back()' , 'clase'=>'', 'style'=>'', 'col'=>'', 'tipocolor'=>'azul', 'icono'=>'regresar','tamanio'=>'pequeño',  'adicional'=>'')
 ));
+$ivagr= ($producto->grabaiva==1)? 'Sí' : 'No';
+$tipoprod= ($producto->tipo==1)? 'PRODUCTO' : 'SERVICIO';
 
 $contenido='<div style="line-height:30px;" class="row"><div class="col-6 col-md-6"><b>Id: </b>'.$producto->id.'<br></div>';
 $contenido.='<div class="col-6 col-md-6"><b>Código: </b>'.$producto->codigo.'<br></div>';
-$contenido.='<div class="col-6 col-md-6"><b>Presentacion:</b>&nbsp; '.$producto->presentacion0->nombre.'</span><br></div>';
 $contenido.='<div class="col-6 col-md-6"><b>Línea:</b>&nbsp; '.$producto->tipoproducto0->nombre.'</span><br></div>';
-$contenido.='<div class="col-6 col-md-6"><b>Color:</b>&nbsp; '.$producto->color0->nombre.'</span><br></div>';
-$contenido.='<div class="col-6 col-md-6"><b>Unidad:</b>&nbsp; '.$producto->tipounidad0->nombre.'</span><br></div>';
 $contenido.='<div class="col-6 col-md-6"><b>Característica:</b>&nbsp; '.$producto->caracteristica0->nombre.'</span><br></div>';
+$contenido.='<div class="col-6 col-md-6"><b>Presentacion:</b>&nbsp; '.$producto->presentacion0->nombre.'</span><br></div>';
+$contenido.='<div class="col-6 col-md-6"><b>Unidad:</b>&nbsp; '.$producto->tipounidad0->nombre.'</span><br></div>';
+$contenido.='<div class="col-6 col-md-6"><b>Color:</b>&nbsp; '.$producto->color0->nombre.'</span><br></div>';
+$contenido.='<div class="col-6 col-md-6"><b>Graba iva:</b>&nbsp; '.$ivagr.'</span><br></div>';
+$contenido.='<div class="col-6 col-md-6"><b>Tipo:</b>&nbsp; '.$tipoprod.'</span><br></div>';
 //$contenido.='<div class="col-6 col-md-6"><b>Cliente:</b>&nbsp; '.$producto->idcliente0->razonsocial.'</span><br></div>';
 //$contenido.='<div class="col-6 col-md-6"><b>Número:</b>&nbsp; '.$producto->id.'</span><br></div>';
 $contenido.='<div class="col-12 col-md-12"><hr style="color: #0056b2;"></div>';
 $contenido.='<div class="col-12 col-md-12"><b>Nombre:</b>&nbsp; '.$producto->nombreproducto.'</span><br></div>';
-$contenido.='<div class="col-6 col-md-6"><b>Características:</b>&nbsp; '.$producto->caracteristicas.'</span><br></div>';
-$contenido.='<div class="col-12 col-md-12"><hr style="color: #0056b2;"></div>';
-$contenido.='<div class="col-12 col-md-12"><b>producto #</b>&nbsp; '.$productobanco.'</span><br></div>';
-$contenido.='<div class="col-12 col-md-12"><hr style="color: #0056b2;"></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Un. Bulto:</b>&nbsp; '.$producto->unibulto.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Descuento:</b>&nbsp; '.$producto->descuento.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Costo F.O.B:</b>&nbsp; '.$producto->costofob.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Costo Un.:</b>&nbsp; '.$producto->costoini.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Por Mayor:</b>&nbsp; '.$producto->preciomayor.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Distribuidor:</b>&nbsp; '.$producto->precio.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Distribuidor 1:</b>&nbsp; '.$producto->preciodist2.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Distribuidor 2:</b>&nbsp; '.$producto->preciodist3.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>PVP:</b>&nbsp; '.$producto->preciopvp.'</span><br></div>';
+$contenido.='<div class="col-4 col-md-3"><b>Descuento C.:</b>&nbsp; '.$producto->nombreproducto.'</span><br></div>';
+
+//$contenido.='<div class="col-12 col-md-12"><hr style="color: #0056b2;"></div>';
 $contenido.='</div>';
 
 if ($model->estatus=="ACTIVO"){ $stylestatus="badge-success"; }else{ $stylestatus="badge-secondary" ; }
